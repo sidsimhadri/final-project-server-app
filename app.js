9bb30567bf9fc84b34409afba3276d9149751a98
sidsimhadri <sidsimhadri@gmail.com>
2023-04-19 18:52:26 -0400

    online db not working

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ import SpotifyController from './controllers/spotify/spotify-controller.js';
 import session from 'express-session';
 import mongoose from "mongoose";
 dotenv.config()
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
- || 'mongodb://127.0.0.1:27017/trackstar'
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//     || 'mongodb://127.0.0.1:27017/trackstar'
+ const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/trackstar'
 mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(session({
